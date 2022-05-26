589f286b8ebadf8b178a93a4a8e24ea89e8190bb
Marielle <marielle.ibias@example.comgit config --global user.name Mariellegit config --global user.email marielle.ibias@example.com>
2022-05-26 10:33:26 +0200

    navbar bootstrap

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,20 +2,20 @@ import React from "react";
 import "./Menu.css"
 import MenuButton from "./MenuButton";
 import Logo from "../images/logo.png";
-
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 function Menu() {
   return (
-        <nav className= "nav-bar">
-            <div className="logo-bar">
-                <img className= "logo" src={Logo}/>
-                <h4>Soundwave</h4>
-            </div>
 
-            <div className="nav-buttons">
-                <MenuButton buttonText={"Discover"}/>
-                <MenuButton buttonText={"Join"}/>
-            </div>
-        </nav>
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home"><img className= "logo" src={Logo}/></Navbar.Brand>
+  <Navbar.Brand href="#home"><h2>Soundwave</h2></Navbar.Brand>
+    <Nav className="nav-link">
+      <Nav.Link href=".discover">Discover</Nav.Link>
+      <Nav.Link href=".join">Join</Nav.Link>
+    </Nav>
+  </Container>
+</Navbar>
   );
 }
 
