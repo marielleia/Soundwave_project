fe9d6c89682cd9ae4bd369c37596fb9a8c790117
Marielle <marielle.ibias@example.comgit config --global user.name Mariellegit config --global user.email marielle.ibias@example.com>
2022-05-28 18:55:10 +0200

    update css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,15 @@ import Twitter from "../images/twitter.svg";
 function Footer() {
   return (
     <footer>
-        <div className='contacto'>
+      <div className='contacto'>
           <FooterButton buttonText={"About"}/>
           <FooterButton buttonText={"Contact Us"}/>
         </div>
         <div className='social-media'>
-          <img src={Twitter} className= "twitter" alt="twitter"/><h6>Twitter</h6>
-          <img src={Facebook} className= "facebook" alt="facebook"/><h6>Facebook</h6>
+          <img src={Twitter} className="twitter" alt="twitter"/><h6>Twitter</h6>
+          <img src={Facebook} className="facebook" alt="facebook"/><h6>Facebook</h6>
         </div>
     </footer>
   );
 }
-
 export default Footer;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,14 @@
 import React from "react";
-import "./Menu.css"
-import Logo from "../images/logo.png";
 import { Navbar, Nav, Container} from "react-bootstrap";
-
+import Logo from "../images/logo.png";
+import "./Menu.css";
 
 function Menu() {
   return (
   <nav>
-    <Navbar sticky="top" expand="lg"  bg="dark" variant="dark">
+    <Navbar expand="lg"  bg="dark" variant="dark" sticky="top">
       <Container>
-        <Navbar.Brand href="#SoundWave"><img className= "logo" src={Logo}/> SoundWave 
+        <Navbar.Brand href=".SoundWave"><img className= "logo" alt="soundwave logo" src={Logo}/> SoundWave 
         </Navbar.Brand>
           <Nav className="nav-link">
             <Nav.Link href=".discover">Discover</Nav.Link> 
